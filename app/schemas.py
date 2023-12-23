bc8cdfa60a98fb6aadc5cb57934c26f22f9f4c0b
Aditya Keshari <68159582+aditya292002@users.noreply.github.com>
2023-12-23 07:14:07 +0000

    codespace setup

--- app/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-from typing import Optional
-
-class PostBase(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
